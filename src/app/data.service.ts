58cfe9e3b0a9e649603b33474397d9497599c64a
Omkar_Warule <129866605+omkarwarule@users.noreply.github.com>
2025-02-14 09:32:42 +0530

    Update data.service.ts  with jee

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,13 +9,21 @@ export class DataService
 {
   private apiUrl = 'http://localhost:3000/data'; 
   private apiUrl2 = 'http://localhost:3000/api/colleges';
+  private apiUrl3 = 'http://localhost:3000/api/jee';
+  
+
   constructor(private http: HttpClient) {}
   getData(): Observable<any> 
   {
     return this.http.get<any>(this.apiUrl);
   }
-  getFilteredColleges(caste: string, percentile: number, branch:string[],locations: string[]): Observable<any[]> 
+  getFilteredColleges(caste: string, percentile: number, branch:string[],locations: string[],minority:string[]): Observable<any[]> 
   {
-    return this.http.post<any[]>(this.apiUrl2, { caste, percentile, branch,locations });
+    return this.http.post<any[]>(this.apiUrl2, { caste, percentile, branch,locations,minority });
   }
+  getFilteredCollegesj(percentile: any, branch:string[],locations: string[]): Observable<any[]> 
+  {
+    return this.http.post<any[]>(this.apiUrl3, {  percentile, branch,locations, });
+  }
+  
 }
